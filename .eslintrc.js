334b0d31196ffccbc64c362f325816daa33d2dc3
Asher Dale <asher@dales.org>
2020-03-22 14:20:10 -0400

    Add eslint packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb-base',
+    'plugin:react/recommended',
+  ],
+  'rules': {
+    'linebreak-style': 0,
+    'prettier/prettier': 0
+  }
 };
